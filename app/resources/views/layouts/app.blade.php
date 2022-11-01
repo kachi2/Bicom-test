62aa2ecbb203428f160bcf4401430c57e8cd228b
Michael Kachi <jesmikky@gmail.com>
2022-11-01 20:35:23 +0100

    initial commit

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
     <script src="{{asset('/asset/js/charts/gd-analytics.js?ver=2.2.0 ')}}"></script>
     <script src="{{asset('/asset/js/libs/jqvmap.js?ver=2.2.0 ')}}"></script>
     @yield('scripts')
+    
 </body>
 
 </html>
